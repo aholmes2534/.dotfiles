f3036c4456b7b23832106f88e09bc5617dfdf355
Tim Freedman <tim3dman@gmail.com>
2022-08-02 23:01:16 +1000

    updated init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,11 +15,12 @@ opt.ignorecase = true
 opt.cursorline = true
 opt.smartcase = true
 opt.hlsearch = false
-opt.wrap = true
 opt.breakindent = true
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = false
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
 opt.undofile = true
 opt.mouse = "nv"
 opt.scrolloff = 4
@@ -32,28 +33,6 @@ g.netrw_winsize = 30
 g.loaded_perl_provider = 0
 
 g.mapleader = ' '
-
-key.set({'n', 'x'}, 'cp', '"+y')
-key.set({'n', 'x'}, 'cv', '"+p')
-key.set({'n', 'x'}, 'x', '"_x')
-key.set('n', '<leader>w', '<cmd>write<cr>')
-key.set('n', '<leader>a', ':keepjumps normal! ggVG<CR>')
-
-local group = vim.api.nvim_create_augroup('user_cmds', {clear = true})
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight on yank',
-  group = group,
-  callback = function()
-    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
-  end,
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = {'help', 'man'},
-  group = group,
-  command = 'nnoremap <buffer> q <cmd>quit<cr>'
-})
 
 -- ================================================================== --
 -- ===                          PLUGINS                           === --
@@ -80,16 +59,12 @@ vim.api.nvim_create_user_command(
 
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'jiangmiao/auto-pairs'
+  use 'preservim/nerdtree'
+  use 'preservim/nerdcommenter'
+  use 'norcalli/nvim-colorizer.lua'
   use 'joshdick/onedark.vim'
   use 'nvim-lualine/lualine.nvim'
-	use 'ellisonleao/gruvbox.nvim'
-	use {
-    'goolord/alpha-nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
-}
 
   if install_plugins then
     require('packer').sync()
@@ -105,39 +80,11 @@ end
 -- ===                    PLUGIN CONFIGURATION                    === --
 -- ================================================================== --
 
----
--- Colorschemes
----
-
 --- Onedark
---opt.termguicolors = true
---cmd('colorscheme onedark')
+opt.termguicolors = true
+cmd('colorscheme onedark')
 
---- Gruvbox
--- setup must be called before loading the colorscheme
--- Default options:
-
-require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = false,
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  overrides = {},
-})
-vim.o.background = "dark" -- light or dark mode
-cmd("colorscheme gruvbox")
-
----
 -- lualine.nvim (statusline)
----
-
 opt.showmode = false
 require('lualine').setup({
   options = {
